a0e445009585ad7e8751fe09f6d273a5e9c25c9d
Tom Kadwill <tomkadwill@gmail.com>
2016-10-12 06:45:32 +0100

    Add next post and remove extra job_site application

--- 1475366400_null_object_pattern/job_site/app/controllers/candidates_controller.rb
+++
@@ -1,9 +0,0 @@
-class CandidatesController < ApplicationController
-  def index
-    @candidates = Candidate.all
-    @candidate_of_the_week = Candidate.find_by(name: 'blah') || NullCandidate.new
-  end
-
-  def search
-  end
-end
